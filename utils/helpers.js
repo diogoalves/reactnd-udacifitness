dda1e8cae82e3fbab7f6b1b57b362fc85a909427
casa <casa@casas-iMac.lan>
2018-06-28 21:38:18 -0300

    finished sample project

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
-import { white, red, orange, blue, lightPurp, pink } from './colors';
+import React from 'react'
+import { View, StyleSheet, AsyncStorage } from 'react-native'
+import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
+import { red, orange, blue, lightPurp, pink, white } from './colors'
+import { Notifications, Permissions } from 'expo'
 
-const styles =  StyleSheet.create({
+const NOTIFICATION_KEY = 'UdaciFitness:notifications'
+
+export function getDailyReminderValue () {
+  return {
+    today: "👋 Don't forget to log your data today!"
+  }
+}
+
+const styles = StyleSheet.create({
   iconContainer: {
     padding: 5,
     borderRadius: 8,
@@ -12,14 +21,8 @@ const styles =  StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginRight: 20
-  }
-});
-
-export function getDailyReminderValue () {
-  return {
-    today: "👋 Don't forget to log your data today!"
-  }
-}
+  },
+})
 
 export function getMetricMetaInfo (metric) {
   const info = {
@@ -120,6 +123,7 @@ export function getMetricMetaInfo (metric) {
     : info[metric]
 }
 
+
 export function isBetween (num, x, y) {
   if (num >= x && num <= y) {
     return true
@@ -160,4 +164,55 @@ export function timeToString (time = Date.now()) {
   const date = new Date(time)
   const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
   return todayUTC.toISOString().split('T')[0]
+}
+
+export function clearLocalNotification () {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY)
+    .then(Notifications.cancelAllScheduledNotificationAsync)
+}
+
+function createNotification () {
+  return {
+    title: 'Log your stats!',
+    body: "👋 don't forget to log your stats for today!",
+    ios: {
+      sound: true,
+    },
+    android: {
+      sound: true,
+      priority: 'high',
+      sticky: false,
+      vibrate: true,
+    }
+  }
+}
+
+export function setLocalNotification () {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then((data) => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+          .then(({ status }) => {
+            if (status === 'granted') {
+              Notifications.cancelAllScheduledNotificationAsync()
+
+              let tomorrow = new Date()
+              tomorrow.setDate(tomorrow.getDate() + 1)
+              tomorrow.setHours(20)
+              tomorrow.setMinutes(0)
+
+              Notifications.scheduleLocalNotificationAsync(
+                createNotification(),
+                {
+                  time: tomorrow,
+                  repeat: 'day',
+                }
+              )
+
+              AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+            }
+          })
+      }
+    })
 }
